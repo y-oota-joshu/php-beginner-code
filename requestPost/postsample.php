cff4e65b149d6bf39d4ca051fcbfb87f8f20c0e0
y-oota-joshu-sys <y-oota@joshu-sys.com>
2021-07-26 22:56:50 +0900

    【PHP超入門】POSTリクエストの使い方 の開設追加

---
+++ requestPost/postsample.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+  <meta http-equiv="content-type" charset="UTF-8" />
+</head>
+
+<body>
+  <div>
+    送られた POSTの「nicname」は <?php echo $_POST["nicname"]; ?> です。
+
+    <?php
+    // 動画では、下記のように記載しています。
+    /*
+      echo "送られた POSTの「nicname」は ". $_POST["nicname"]. " です。";
+    */
+    ?>
+
+  </div>
+</body>
+
+</html>
